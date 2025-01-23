79da338b669469515d916f33942c7be910e9bb6e
Tobi Okanlawon <tobiinlondon34@gmail.com>
2025-01-23 15:50:05 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,27 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `The Eternal Sacred Order of the Cherubim and Seraphim`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "\u0016https://super-vault.esocs.net/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
