644b365cc1ab4026e9e831c87305c2ae06dc099c
Tobi Okanlawon <tobiinlondon34@gmail.com>
2025-02-01 11:19:17 +0100

    Deactivate watchword container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const IndexPage: React.FC<PageProps> = ({ data }) => {
       <IconCards data={iconCards} />
       <WelcomeToChurch {...wtc} image={wtc.image} />
       <Events data={cleanEvents(events)} />
-      <Watchword />
+      {false && <Watchword />}
       {false && <News data={[]} />}
       <SowingSeeds />
       <WatchAndListenAgain />
