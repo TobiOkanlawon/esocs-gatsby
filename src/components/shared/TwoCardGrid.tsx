7fff15234139fafcb2f0d968164e832079ad46c1
Tobi Okanlawon <tobiinlondon34@gmail.com>
2025-02-04 19:05:56 +0100

    Create TwoCardGrid

---
+++ src/components/shared/TwoCardGrid.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import breakPoints from "../../constants/breakPoints";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const StyledContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(540px, 1fr));
+  grid-gap: 12px;
+  justify-items: center;
+
+  @media (max-width: ${breakPoints.smallScreen}) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const TwoCardGrid: React.FC<Props> = ({ children }) => {
+  return <StyledContainer>{children}</StyledContainer>;
+};
+
+export default TwoCardGrid;
